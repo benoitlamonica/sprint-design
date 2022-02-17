33a7818bbff9fe05a276c2b6ad123784a684d83c
teyz <contact@bastienrigaud.fr>
2022-02-17 14:59:13 +0100

    feat: add OnboardingPopup

--- src/components/OnboardingPopup.vue
+++ src/components/OnboardingPopup.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="absolute bottom-0 bg-onboarding">
-    <div class="flex flex-col justify-center items-center p-10">
+    <div class="flex flex-col justify-center items-center p-8">
       <img
         src="@/assets/horse.svg"
         alt=""
       >
-      <h2 class="text-white">
+      <h2 class="text-white mt-2 font-bold text-xl">
         Besoin d’aide ?
       </h2>
       <p class="text-white text-center mt-2 mb-2">
         Premier paris sur une course hippique ? Réalisons un pari fictif ensemble 🐴
       </p>
+      <div class="flex w-full mt-4 justify-between">
+        <button class="text-white border border-solid border-white rounded-full w-36 p-3">
+          Plus tard
+        </button>
+        <button class="text-white border border-solid border-red rounded-full p-3 w-36 bg-red">
+          C’est parti !
+        </button>
+      </div>
     </div>
   </div>
 </template>
