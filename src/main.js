6794e5f83a2f0dcf824dcc27ba7080536c8c4292
Felix <bouveret.felix@gmail.com>
2022-02-17 14:14:19 +0100

    core(i18n): add i18n config

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import './css/_tailwind.css';
+import i18n from './i18n';
 
-createApp(App).mount('#app');
+createApp(App).use(i18n).mount('#app');
