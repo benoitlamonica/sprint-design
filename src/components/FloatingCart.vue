f67a3dd6cc046c5c871e7f6e9b0fc364c4f31299
Felix <bouveret.felix@gmail.com>
2022-02-17 14:01:27 +0100

    fix(FloatingCart): fix lint

--- src/components/FloatingCart.vue
+++ src/components/FloatingCart.vue
@@ -2,7 +2,10 @@
   <div
     class="w-20 h-20 bg-red rounded-full items-center justify-center flex fixed right-4 bottom-4"
   >
-    <img src="@/assets/cart.svg" alt="Cart" />
+    <img
+      src="@/assets/cart.svg"
+      alt="Cart"
+    />
   </div>
 </template>
 
