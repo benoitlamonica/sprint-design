b6e17cb9a78b2bcbf63135355b6132150a6178b7
Felix <bouveret.felix@gmail.com>
2022-02-17 13:50:59 +0100

    feat(Home): first sections

---
+++ src/components/Home/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <img
+    class="fixed h-24 z-10"
+    :src="header"
+  />
+  <div class="grid grid-cols-1 gap-2.5 top-24 relative pt-2.5 px-2.5">
+    <img
+      v-for="(section, index) in sections"
+      :key="index"
+      :src="section"
+      class="block"
+    />
+  </div>
+</template>
+
+<script>
+import { useSections } from './useSections';
+
+export default {
+  name: 'Home',
+
+  setup() {
+    const { header, sections } = useSections();
+
+    return {
+      sections,
+      header
+    };
+  } 
+};
+</script>
